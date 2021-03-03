cffc1395bbefb95e96dcdaafd757483a58cc09a1
quoc le <dinhquoc0103@gmail.com>
2021-03-03 16:52:59 +0700

    test index.php

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
 <?php
 
-    // Gọi các file định nghĩa sẵn các keyword
-    require_once "define.php";
+    // // Gọi các file định nghĩa sẵn các keyword
+    // require_once "define.php";
 
-    date_default_timezone_set('Asia/Ho_Chi_Minh');
+    // date_default_timezone_set('Asia/Ho_Chi_Minh');
 
-    // Tự động load các class dùng chung ở folder libs khi cần
-    spl_autoload_register(function($className){
-        $filePath = LIBRARY_PATH . $className . ".php";
-        if(file_exists($filePath)){
-            require_once $filePath;  
-        }
-    });
-    // Session start
-    Session::init();
+    // // Tự động load các class dùng chung ở folder libs khi cần
+    // spl_autoload_register(function($className){
+    //     $filePath = LIBRARY_PATH . $className . ".php";
+    //     if(file_exists($filePath)){
+    //         require_once $filePath;  
+    //     }
+    // });
+    // // Session start
+    // Session::init();
 
-    // Tạo 1 đối tượng bootstrap để chuyển hướng đến đúng module, controller, action muốn thực hiện
-    $bt = new Bootstrap();
-    $bt->init();
+    // // Tạo 1 đối tượng bootstrap để chuyển hướng đến đúng module, controller, action muốn thực hiện
+    // $bt = new Bootstrap();
+    // $bt->init();
+
+    echo 'haha';
 ?>
