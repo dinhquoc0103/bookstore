6bed18fdf33a8b361c1b495cee46aa223ca6cf96
quoc le <dinhquoc0103@gmail.com>
2021-03-04 17:32:01 +0700

    fix send mail for check-out and register

--- application/module/site/controllers/CartController.php
+++ application/module/site/controllers/CartController.php
@@ -140,27 +140,29 @@
                     //     die();
                     // }
                     // exit();
-                    require CORE_PATH . 'sendGrid/vendor/autoload.php';
-                    $key = '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';
-                    $email = new \SendGrid\Mail\Mail();
-                    $email->setFrom("dinhquoc0103@gmail.com", "bookstore");
-                    $email->setSubject('test email');
-                    $email->addTo('spidermandog.k6fc@gmail.com', 'ldquoc');
-                    $email->addContent("text/html", "<h1>Thành công rồi hihi hahahaha</h1>");
+                    // require CORE_PATH . 'sendGrid/vendor/autoload.php';
+                    // $key = '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';
+                    // $email = new \SendGrid\Mail\Mail();
+                    // $email->setFrom("dinhquoc0103@gmail.com", "bookstore");
+                    // $email->setSubject('test email');
+                    // $email->addTo('spidermandog.k6fc@gmail.com', 'ldquoc');
+                    // $email->addContent("text/html", "<h1>Thành công rồi hihi hahahaha</h1>");
                 
-                    $sendgrid = new \SendGrid($key);
+                    // $sendgrid = new \SendGrid($key);
                 
-                    try{
-                        $response = $sendgrid->send($email);
-                        print $response->statusCode() . "\n";
-                        echo '<pre>';
-                        print_r($response->headers());
-                        print $response->body() . "\n";
-                    }
-                    catch(Exception $e){
-                        echo "Email exception Caught : " . $e->getMessage() . "\n";
-                        return false;
-                    }
+                    // try{
+                    //     $response = $sendgrid->send($email);
+                    //     print $response->statusCode() . "\n";
+                    //     echo '<pre>';
+                    //     print_r($response->headers());
+                    //     print $response->body() . "\n";
+                    // }
+                    // catch(Exception $e){
+                    //     echo "Email exception Caught : " . $e->getMessage() . "\n";
+                    //     return false;
+                    // }
+
+                    echo 'vl chưa'; die();
 
                 }
                 else{
