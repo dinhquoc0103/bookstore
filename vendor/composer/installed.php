5c2c38daf460506889dc82a59bc31fa1f4c47e4c
quoc le <dinhquoc0103@gmail.com>
2022-10-08 12:14:18 +0700

    create and install composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e12485c6c7312c256f70f880e54af28f56210b34',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e12485c6c7312c256f70f880e54af28f56210b34',
+    ),
+  ),
+);
